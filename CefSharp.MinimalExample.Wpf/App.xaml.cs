8466e06edf6212213504c9d624d542e9e127ac25
Huang Chen <huangchen01@fenbi.com>
2019-08-06 19:13:18 +0800

    MOD: change minimal example to reflect our crash

--- CefSharp.MinimalExample.Wpf/App.xaml.cs
+++ CefSharp.MinimalExample.Wpf/App.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace CefSharp.MinimalExample.Wpf
 {
@@ -9,22 +10,93 @@ namespace CefSharp.MinimalExample.Wpf
     {
         public App()
         {
-            //Monitor parent process exit and close subprocesses if parent process exits first
-            //This will at some point in the future becomes the default
-            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
+        }
 
-            var settings = new CefSettings()
-            {
-                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
-                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
-            };
-            
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            MyBrowserProcessHandler.UIDispatcher = Dispatcher;
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+
+            var browserProcessHandler = new MyBrowserProcessHandler();
+            var settings = new CefSettings();
+            settings.UserAgent = $"CefSharp Browser/{Cef.CefSharpVersion} OurProductVersion";
+            settings.PersistSessionCookies = true;
+
             //Example of setting a command line argument
             //Enables WebRTC
-            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+            //settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
 
             //Perform dependency check to make sure all relevant resources are in our output directory.
-            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: browserProcessHandler);
+        }
+    }
+
+    public class MyBrowserProcessHandler : IBrowserProcessHandler
+    {
+        // MVVMLite's UIDispatcher is used in our code
+        public static Dispatcher UIDispatcher { get; set; }
+
+        public void OnContextInitialized()
+        {
+            SetCookies();
+        }
+
+        public void OnScheduleMessagePumpWork(long delay)
+        {
+            // Not Implemented
+        }
+
+        public void Dispose()
+        {
+            // Not Implemented
+        }
+
+        public static void SetCookies()
+        {
+            RunInUIThread(
+                () =>
+                {
+                    SetCookie("persistent", "testVal");
+                    SetCookie("sess", "testVal");
+                    SetCookie("userid", "testVal");
+                });
+        }
+
+        public static void SetCookie(string name, string value)
+        {
+            var cookie = new Cookie
+            {
+                Name = name,
+                Value = value,
+                Creation = DateTime.Now,
+                Domain = ".our.domain.com"
+            };
+
+            DeleteCookieSync("https://.our.domain.com", name);
+            SetCookieSync("https://.our.domain.com", cookie);
+        }
+
+        public static void DeleteCookieSync(string url, string name)
+        {
+            var sucess = Cef.GetGlobalCookieManager().DeleteCookiesAsync(url, name).Result;
+        }
+
+        public static void SetCookieSync(string url, Cookie cookie)
+        {
+            var sucess = Cef.GetGlobalCookieManager().SetCookieAsync(url, cookie).Result;
+        }
+
+        public static void RunInUIThread(Action method)
+        {
+            if (UIDispatcher.CheckAccess())
+            {
+                method.Invoke();
+            }
+            else
+            {
+                UIDispatcher.BeginInvoke(method);
+            }
         }
     }
 }
--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+using System.Windows.Media;
+using CefSharp.Wpf;
 
 namespace CefSharp.MinimalExample.Wpf
 {
@@ -7,6 +11,260 @@ namespace CefSharp.MinimalExample.Wpf
         public MainWindow()
         {
             InitializeComponent();
+            var browser = new CustomCefSharpBrowser("about:blank");
+            BrowserHolder.Content = browser;
+        }
+    }
+
+    public class CustomCefSharpBrowser : ChromiumWebBrowser
+    {
+        public CustomCefSharpBrowser(string url)
+        {
+            Address = url;
+            RenderOptions.SetBitmapScalingMode(this, BitmapScalingMode.NearestNeighbor);
+            if (Cef.IsInitialized)
+            {
+                MyBrowserProcessHandler.SetCookies();
+            }
+
+            this.RegisterAsyncJsObject("OurObject", new CustomJsObject());
+            BrowserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            BrowserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            BrowserSettings.WebSecurity = CefState.Disabled;
+            RequestHandler = new WebViewRequestBaseHandler();
+            MenuHandler = new MenuHandler();
+        }
+    }
+
+    public class CustomJsObject : IDisposable
+    {
+        public void Dispose()
+        {
+            // Do nothing
+        }
+    }
+
+    public class WebViewRequestBaseHandler : IRequestHandler
+    {
+        public bool CanGetCookies(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request)
+        {
+            return true;
+        }
+
+        public bool CanSetCookie(
+            IWebBrowser chromiumWebBrowser,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            Cookie cookie)
+        {
+            return true;
+        }
+
+        public void OnResourceRedirect(
+            IWebBrowser chromiumWebBrowser,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            IResponse response,
+            ref string newUrl)
+        {
+        }
+
+        public void OnResourceRedirect(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            ref string newUrl)
+        {
+        }
+
+        public bool OnSelectClientCertificate(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            bool isProxy,
+            string host,
+            int port,
+            X509Certificate2Collection certificates,
+            ISelectClientCertificateCallback callback)
+        {
+            return false;
+        }
+
+        public bool OnBeforeBrowse(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            bool userGesture,
+            bool isRedirect)
+        {
+            return false;
+        }
+
+        public bool OnBeforeBrowse(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            bool isRedirect)
+        {
+            return false;
+        }
+
+        public bool OnOpenUrlFromTab(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            string targetUrl,
+            WindowOpenDisposition targetDisposition,
+            bool userGesture)
+        {
+            return false;
+        }
+
+        public bool OnCertificateError(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            CefErrorCode errorCode,
+            string requestUrl,
+            ISslInfo sslInfo,
+            IRequestCallback callback)
+        {
+            return false;
+        }
+
+        public void OnPluginCrashed(IWebBrowser browserControl, IBrowser browser, string pluginPath)
+        {
+        }
+
+        public virtual CefReturnValue OnBeforeResourceLoad(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            IRequestCallback callback)
+        {
+            var headers = request.Headers;
+            if (headers["Origin"]?.StartsWith("file") == true)
+            {
+                headers.Remove("Origin");
+                request.Headers = headers;
+            }
+
+            return CefReturnValue.Continue;
+        }
+
+        public bool GetAuthCredentials(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            bool isProxy,
+            string host,
+            int port,
+            string realm,
+            string scheme,
+            IAuthCallback callback)
+        {
+            return false;
+        }
+
+        public virtual void OnRenderProcessTerminated(IWebBrowser browserControl, IBrowser browser, CefTerminationStatus status)
+        {
+        }
+
+        public bool OnQuotaRequest(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            string originUrl,
+            long newSize,
+            IRequestCallback callback)
+        {
+            return false;
+        }
+
+        public bool OnProtocolExecution(IWebBrowser browserControl, IBrowser browser, string url)
+        {
+            return false;
+        }
+
+        public void OnRenderViewReady(IWebBrowser browserControl, IBrowser browser)
+        {
+        }
+
+        public bool OnResourceResponse(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            IResponse response)
+        {
+            return false;
+        }
+
+        public IResponseFilter GetResourceResponseFilter(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            IResponse response)
+        {
+            return null;
+        }
+
+        public void OnResourceLoadComplete(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            IResponse response,
+            UrlRequestStatus status,
+            long receivedContentLength)
+        {
+        }
+    }
+
+    public class MenuHandler : IContextMenuHandler
+    {
+        public void OnBeforeContextMenu(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IContextMenuParams parameters,
+            IMenuModel model)
+        {
+            model.Clear();
+        }
+
+        public bool OnContextMenuCommand(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IContextMenuParams parameters,
+            CefMenuCommand commandId,
+            CefEventFlags eventFlags)
+        {
+            return false;
+        }
+
+        public void OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+        {
+        }
+
+        public bool RunContextMenu(
+            IWebBrowser browserControl,
+            IBrowser browser,
+            IFrame frame,
+            IContextMenuParams parameters,
+            IMenuModel model,
+            IRunContextMenuCallback callback)
+        {
+            return false;
         }
     }
 }
