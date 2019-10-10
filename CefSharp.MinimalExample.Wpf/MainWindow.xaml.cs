b562fba02479ce853defb814b33b877bc7be4215
Huang Chen <huangchen01@fenbi.com>
2019-10-10 21:05:42 +0800

    Create crash sanario
    change browser height from h=0 to h>0 while browser is detached from
    visual tree. on reattachment to visual tree, browser crashes.

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,12 +1,62 @@
 ﻿using System.Windows;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using CefSharp.Wpf;
 
 namespace CefSharp.MinimalExample.Wpf
 {
     public partial class MainWindow : Window
     {
+        private ChromiumWebBrowser _bingBrowser;
+
+        private ChromiumWebBrowser _googleBrowser;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            _bingBrowser?.Dispose();
+            _bingBrowser = new ChromiumWebBrowser("www.bing.com");
+            BingBrowserContainer.Content = _bingBrowser;
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _bingBrowser?.Dispose();
+            _googleBrowser?.Dispose();
+            _bingBrowser = null;
+            _googleBrowser = null;
+        }
+
+        private async void Crash_Click(object sender, RoutedEventArgs e)
+        {
+            var uiDispatcher = Dispatcher;
+
+            _googleBrowser?.Dispose();
+            _googleBrowser = new ChromiumWebBrowser("www.google.com");
+            _googleBrowser.Height = 0;
+            GoogleBrowserContainer.Content = _googleBrowser;
+            await Task.Delay(1000);
+            MyTabControl.SelectedIndex = 1;
+            await Task.Delay(1000);
+            if (_googleBrowser != null)
+            {
+                _googleBrowser.Height = 600;
+            }
+
+            await Task.Delay(1000);
+            MyTabControl.SelectedIndex = 0;
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            MyTabControl.SelectedIndex = 0;
+            _googleBrowser?.Dispose();
+            _googleBrowser = null;
         }
     }
 }
