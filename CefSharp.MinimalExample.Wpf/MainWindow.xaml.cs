8843dc93972500d6ae4eb0a88e2d95ceaf9fa8a6
Huang Chen <huangchen01@fenbi.com>
2019-10-10 22:15:32 +0800

    Simplify crash condition

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -8,8 +8,6 @@ namespace CefSharp.MinimalExample.Wpf
 {
     public partial class MainWindow : Window
     {
-        private ChromiumWebBrowser _bingBrowser;
-
         private ChromiumWebBrowser _googleBrowser;
 
         public MainWindow()
@@ -17,46 +15,22 @@ namespace CefSharp.MinimalExample.Wpf
             InitializeComponent();
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-            _bingBrowser?.Dispose();
-            _bingBrowser = new ChromiumWebBrowser("www.bing.com");
-            BingBrowserContainer.Content = _bingBrowser;
-        }
-
         private void Page_Unloaded(object sender, RoutedEventArgs e)
         {
-            _bingBrowser?.Dispose();
             _googleBrowser?.Dispose();
-            _bingBrowser = null;
             _googleBrowser = null;
         }
 
         private async void Crash_Click(object sender, RoutedEventArgs e)
         {
-            var uiDispatcher = Dispatcher;
-
             _googleBrowser?.Dispose();
             _googleBrowser = new ChromiumWebBrowser("www.google.com");
             _googleBrowser.Height = 0;
             GoogleBrowserContainer.Content = _googleBrowser;
             await Task.Delay(1000);
-            MyTabControl.SelectedIndex = 1;
+            GoogleBrowserContainer.Content = 123;
             await Task.Delay(1000);
-            if (_googleBrowser != null)
-            {
-                _googleBrowser.Height = 600;
-            }
-
-            await Task.Delay(1000);
-            MyTabControl.SelectedIndex = 0;
-        }
-
-        private void Reset_Click(object sender, RoutedEventArgs e)
-        {
-            MyTabControl.SelectedIndex = 0;
-            _googleBrowser?.Dispose();
-            _googleBrowser = null;
+            GoogleBrowserContainer.Content = _googleBrowser;
         }
     }
 }
